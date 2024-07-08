3eb3cb8f5bfdc53b3ebac737c5854ccd3915243a
Gilad Kutiel <giladk@mobileye.com>
2024-07-08 13:33:29 +0300

    landmark detection

--- webcam/__init__.py
+++ webcam/__init__.py
@@ -1,11 +1,22 @@
 import cv2
+import mediapipe as mp
 import uvicorn
 from fastapi import FastAPI, WebSocket
-from fastapi.responses import StreamingResponse
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
-app = FastAPI(debug=True)
+base_options = python.BaseOptions('face_landmarker_v2_with_blendshapes.task')
+options = vision.FaceLandmarkerOptions(
+    base_options=base_options,
+    output_face_blendshapes=False,
+    output_facial_transformation_matrixes=False,
+    num_faces=1)
+
+fld_detector = vision.FaceLandmarker.create_from_options(options)
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+app = FastAPI(debug=True)
 
 
 def frames():
@@ -14,16 +25,27 @@ def frames():
     while True:
         _, frame = cap.read()
         h, w, *_ = frame.shape
-
-        frame = cv2.resize(frame, (w//4, h//4))
-
+        h, w = h//2, w//2
+        frame = cv2.resize(frame, (w, h))
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
         for (x, y, w, h) in faces:
             # To draw a rectangle in a face
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
+            face = frame[y:y+h, x:x+w, :].astype('uint8')
+
+            img = mp.Image(
+                image_format=mp.ImageFormat.SRGB,
+                data=face)
+
+            res = fld_detector.detect(img)
+            if res.face_landmarks:
+                for landmarks in res.face_landmarks:
+                    for landmark in landmarks:
+                        fx, fy = landmark.x, landmark.y
+                        fx, fy = int(fx*w), int(fy*h)
+                        cv2.circle(frame, (x + fx, y + fy), 2, (0, 255, 0), -1)
 
         yield frame
 
