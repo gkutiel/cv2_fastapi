c063ca606f77cc72e2ec4c092b4c953138da1a85
Gilad Kutiel <giladk@mobileye.com>
2024-07-08 12:49:35 +0300

    face detection

--- webcam/__init__.py
+++ webcam/__init__.py
@@ -5,12 +5,23 @@ from fastapi.responses import StreamingResponse
 
 app = FastAPI(debug=True)
 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 
 def frames():
     cap = cv2.VideoCapture(0)
 
     while True:
         _, frame = cap.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            # To draw a rectangle in a face
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
+
         yield frame
 
 
